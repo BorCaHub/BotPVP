bf493554f4197868f081936d906ebf1dbfec2238
BorcaBOYYY <xenofilsg@gmail.com>
2026-06-20 14:41:02 +0700

    Update BotEntity.java

--- src/main/java/com/botpvp/bot/BotEntity.java
+++ src/main/java/com/botpvp/bot/BotEntity.java
@@ -1,29 +1,21 @@
 package com.botpvp.bot;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BotEntity extends Monster {
 
     private final PvPBot pvpBot;
-    private final ItemStack[] equipment = new ItemStack[6];
 
     public BotEntity(ServerLevel world, PvPBot pvpBot) {
         super(EntityType.ZOMBIE, world);
         this.pvpBot = pvpBot;
-        for (int i = 0; i < equipment.length; i++) equipment[i] = ItemStack.EMPTY;
     }
 
     public static AttributeSupplier.Builder createBotAttributes() {
@@ -48,18 +40,6 @@ public class BotEntity extends Monster {
     public boolean isUndead() { return false; }
 
     @Override
-    public ItemStack getItemBySlot(EquipmentSlot slot) {
-        int idx = slot.getIndex();
-        return (idx >= 0 && idx < equipment.length) ? equipment[idx] : ItemStack.EMPTY;
-    }
-
-    @Override
-    public void setItemSlot(EquipmentSlot slot, ItemStack stack) {
-        int idx = slot.getIndex();
-        if (idx >= 0 && idx < equipment.length) equipment[idx] = stack;
-    }
-
-    @Override
     protected SoundEvent getHurtSound(DamageSource source) {
         return SoundEvents.PLAYER_HURT;
     }
@@ -67,16 +47,6 @@ public class BotEntity extends Monster {
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.PLAYER_DEATH;
-    }
-
-    @Override
-    public Iterable<ItemStack> getArmorSlots() {
-        List<ItemStack> armor = new ArrayList<>();
-        armor.add(equipment[EquipmentSlot.FEET.getIndex()]);
-        armor.add(equipment[EquipmentSlot.LEGS.getIndex()]);
-        armor.add(equipment[EquipmentSlot.CHEST.getIndex()]);
-        armor.add(equipment[EquipmentSlot.HEAD.getIndex()]);
-        return armor;
     }
 
     public PvPBot getPvpBot() { return pvpBot; }
